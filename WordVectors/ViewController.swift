338728324b71a3c826f414d312990158f4aa7093
Adam Wulf <adam@adamwulf.me>
2026-07-18 12:46:48 -0500

    adding empty project

---
+++ WordVectors/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WordVectors
+//
+//  Created by Adam Wulf on 7/18/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ WordVectorsTests/WordVectorsTests.swift
@@ -0,0 +1,17 @@
+//
+//  WordVectorsTests.swift
+//  WordVectorsTests
+//
+//  Created by Adam Wulf on 7/18/26.
+//
+
+import Testing
+@testable import WordVectors
+
+struct WordVectorsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
